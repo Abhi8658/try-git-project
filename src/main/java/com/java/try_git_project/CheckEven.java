ebe5a0c9c677d5898786c08a0e88479ff81005c2
Abhi8658 <sonu.panda24@gmail.com>
2018-02-18 23:10:42 +0530

    initial commit

---
+++ src/main/java/com/java/try_git_project/CheckEven.java
@@ -0,0 +1,16 @@
+package com.java.try_git_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class CheckEven 
+{
+    public void checkEvenOdd(int number) {
+    	if (number%2==0) {
+			System.out.println("Even no");
+		}else {
+			System.out.println("Odd no");
+		}
+    }
+}
---
+++ src/main/java/com/java/try_git_project/EvenOddTest.java
@@ -0,0 +1,8 @@
+package com.java.try_git_project;
+
+public class EvenOddTest {
+	public static void main(String[] args) {
+		CheckEven checkEven = new CheckEven();
+		checkEven.checkEvenOdd(12);
+	}
+}
